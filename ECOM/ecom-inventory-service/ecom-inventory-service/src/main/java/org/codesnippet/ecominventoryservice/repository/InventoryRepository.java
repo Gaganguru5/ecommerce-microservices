acf523b80d87ed75d60bdd34930e4b6bafb472e7
gagan <gaganmr45@gmail.com>
2026-04-27 23:47:28 +0530

    new commit

--- ECOM/ecom-inventory-service/ecom-inventory-service/src/main/java/org/codesnippet/ecominventoryservice/repository/InventoryRepository.java
+++
@@ -1,9 +0,0 @@
-package org.codesnippet.ecominventoryservice.repository;
-
-import org.codesnippet.ecominventoryservice.model.Inventory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface InventoryRepository extends JpaRepository<Inventory,Long> {
-}
